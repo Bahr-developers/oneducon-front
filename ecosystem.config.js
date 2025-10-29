cd95208f3a750620a5ec5f6ac3309f4e1a8d9357
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2025-10-29 09:55:58 +0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,15 @@
 export default {
   apps: [{
     name: 'frontend',
-    script: 'npm',
-    args: 'start',
+    script: 'serve',
+    args: '-s dist -l 3000',
     cwd: '/var/www/onedukon/front',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'production',
-      PORT: 3000
+      NODE_ENV: 'production'
     },
     error_file: './logs/err.log',
     out_file: './logs/out.log',
