69663f7130d0e5f7974bf9bc1d9ffe7601d04e00
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-11-02 00:36:04 +0500

    adds

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,10 @@
 // src/store/index.ts
 import { configureStore } from "@reduxjs/toolkit";
-import saleReducer from "./sales-store";
-import orderPaymentsSlice from './payments-store';
-import debts from './debts-slice';
-import orderReducer from './order-slice'; // YANGI QO'SHILGAN
+import orderReducer from './order-slice';
 
 export const store = configureStore({
     reducer: {
-        sale: saleReducer,
-        orderPayments: orderPaymentsSlice,
-        debts: debts,
-        order: orderReducer, // YANGI QO'SHILGAN
+        order: orderReducer,
     },
 });
 
