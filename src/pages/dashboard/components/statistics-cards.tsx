0696b237adae7f49bf9efbfc1a2e7164b74631f5
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2026-07-30 12:38:01 +0500

    fixed

--- src/pages/dashboard/components/statistics-cards.tsx
+++ src/pages/dashboard/components/statistics-cards.tsx
@@ -24,7 +24,7 @@ export function StatisticsCards() {
 	const cash = data?.cashOnHand ?? 0
 
 	return (
-		<div className='grid grid-cols-2 gap-2.5 sm:grid-cols-3 xl:grid-cols-5'>
+		<div className='grid grid-cols-2 gap-2.5 sm:grid-cols-3 xl:grid-cols-4'>
 			<StatCard
 				title='Jami savdo'
 				value={sales}
